f22ee8537b0c5c74dacaf545c15a9d1abdd27ab5
Nik <102063769+NikitaMalevich@users.noreply.github.com>
2024-11-13 14:31:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import re
 
+
 class Parser_elibrary():
 
-    def __init__(self, rsci, name_authors):
+    def __init__(self, rsci):
         self.data_res = pd.DataFrame([])
         self.rsci = rsci
-        self.name_authors = name_authors
 
         self.options = Options()
         self.options.add_argument('--headless')
         self.options.add_argument('--no-sandbox')
         self.options.add_argument('--disable-dev-shm-usage')
 
-        self.service = Service('chromedriver.exe')
+        self.service = Service('chromedriver')
         self.driver = webdriver.Chrome(service=self.service)
 
         self.driver.get("https://elibrary.ru/authors.asp")
@@ -43,14 +43,22 @@ class Parser_elibrary():
         select.select_by_value("RSCI")
 
         search_bar_codevalue.send_keys(str(self.rsci))
+        time.sleep(2)
 
         search_button = self.driver.find_element(by=By.CSS_SELECTOR, value='[onclick="author_search()"]').click()
 
-        link_author = self.driver.find_element(by=By.CSS_SELECTOR,
-                                               value='[title="Список публикаций данного автора в РИНЦ"]').get_property(
-            name='href')
-        move_to_link = self.driver.find_element(by=By.CSS_SELECTOR,
-                                                value='[title="Список публикаций данного автора в РИНЦ"]').click()
+        try:
+            move_to_link = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                    value='[title="Список публикаций данного автора в РИНЦ"]').click()
+        except:
+            print('Автор не найден, проверьте код РИНЦ')
+            return
+
+        self.name_authors = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                     value='[style="width:540px; margin:0 20px 20px 20px; border:0; padding:0; text-align: center; font-size: 9pt;"]').text
+
+        fin_symb = self.name_authors.find('*')
+        self.name_authors = self.name_authors[:fin_symb]
 
         cp = self.count_publications()
         total_publications, current_publications = cp[0], cp[1]
@@ -59,45 +67,42 @@ class Parser_elibrary():
             all_articles_per_page = self.driver.find_element(by=By.ID, value='restab').text
             all_articles_data = all_articles_per_page.split('\n')[1:]
 
-            count = 0
+            indices = [index for index, item in enumerate(all_articles_data) if re.match(r'^\d+\.$', item)]
 
-            while True:
-                empty_string = len(self.data_res)
-
-                self.data_res.loc[empty_string, 'Название статьи'] = all_articles_data[count + 1]
-                self.data_res.loc[empty_string, 'Авторы'] = all_articles_data[count + 2]
-                self.data_res.loc[empty_string, 'Издание'] = all_articles_data[count + 3]
-                count += 4
-
+            # for i in indices:
+            for i in range(len(indices)):
                 try:
-                    all_articles_data[count+1]
+                    article_data_i = all_articles_data[indices[i]:indices[i + 1]]
                 except:
-                    break
+                    article_data_i = all_articles_data[indices[i]:]
+
+                empty_string = len(self.data_res)
+                self.data_res.loc[empty_string, 'Название статьи'] = article_data_i[1]
+                self.data_res.loc[empty_string, 'Авторы'] = article_data_i[2]
+                self.data_res.loc[empty_string, 'Издание'] = ' '.join(article_data_i[3:])
 
             if current_publications == total_publications:
                 break
             else:
-                next_page_button = self.driver.find_element(by=By.CSS_SELECTOR, value='[title="Следующая страница"]').click()
+                next_page_button = self.driver.find_element(by=By.CSS_SELECTOR,
+                                                            value='[title="Следующая страница"]').click()
                 cp = self.count_publications()
                 total_publications, current_publications = cp[0], cp[1]
 
-                time.sleep(1)
+                time.sleep(3)
 
         self.data_res.index += 1
         print(self.data_res)
 
         self.data_res.to_excel(f'{self.name_authors}.xlsx', sheet_name=self.name_authors)
+        print('Готово, наслаждайтесь!')
 
-# print(f'Введите rsci автора:')
-# rsci = int(input())
-#
-# print(f'Введите Имя Фамилию автора через пробел:')
-# name_authors = str(input())
+# Nikita : 1192741
+# rsci = 1192741
 
-rsci = 1072704
-name_authors = 'Marina Shirokova'
+if __name__ == '__main__':
+    print(f'Введите rsci автора:')
+    rsci = int(input())
 
-pc = Parser_elibrary(rsci, name_authors)
-pc.launch_parser()
-
-print('Готово, наслаждайтесь!')
+    pc = Parser_elibrary(rsci)
+    pc.launch_parser()
